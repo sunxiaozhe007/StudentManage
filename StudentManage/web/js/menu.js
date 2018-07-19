fdff5bbd7a6728642246c794f8fb6af1bdb01b49
sunxiaozhe007 <985160269@qq.com>
2018-07-19 13:53:13 +0800

    js代码

---
+++ StudentManage/web/js/menu.js
@@ -0,0 +1,13 @@
+/**
+ * Created by AAAAAAAA on 2018/6/13.
+ */
+$("div.menulist").hide()
+$("div.menulist").show(1000)
+$("div.top").hide()
+$("div.top").show(1000)
+$("div.bottom").hide()
+$("div.bottom").show(1000)
+$("div.right_m").hide()
+$("div.right_m").show(2000)
+$("div.right_b").hide()
+$("div.right_b").show(3000)
