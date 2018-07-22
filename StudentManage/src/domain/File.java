536539ea4bf026cf0e96e27c215a2233343e717d
sunxiaozhe007 <985160269@qq.com>
2018-07-22 08:24:38 +0800

    文件实体类

---
+++ StudentManage/src/domain/File.java
@@ -0,0 +1,55 @@
+package domain;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Serializable;
+
+public class File implements Serializable {
+
+    private int id;
+    private String name;
+    private String introduce;
+    private MultipartFile file;
+    private String fileName;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIntroduce() {
+        return introduce;
+    }
+
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+
+    public MultipartFile getFile() {
+        return file;
+    }
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
