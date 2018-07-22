c4bd2b592e45f7f411939bac248d489966b99d47
sunxiaozhe007 <985160269@qq.com>
2018-07-22 08:19:58 +0800

    用户实体类

---
+++ StudentManage/src/domain/User.java
@@ -0,0 +1,84 @@
+package domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+    int id;
+    String username;//用户名
+    String password;//密码
+    int userStatus;//用户状态
+    Date createDate;//创建时间
+    String phone;//手机号
+    String code;//验证码
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(int userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", userStatus=" + userStatus +
+                ", createDate=" + createDate +
+                ", phone='" + phone + '\'' +
+                ", code='" + code + '\'' +
+                '}';
+    }
+}
